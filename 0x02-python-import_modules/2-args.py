53e98b7150ab047b6e207b73f9c65c39ca92b3a3
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-11-30 03:58:13 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    length = len(sys.argv) - 1
-    arg = "{} {}{}".format(
-            length, "argument" if len == 1 else "arguments",
-            "." if length == 0 else ":")
-    print(arg)
-    for i in range(1, length + 1):
+    arg_len = len(sys.argv) - 1
+    ar = "{} {}{}".format(
+            arg_len, "argument" if arg_len == 1 else "arguments",
+            "." if arg_len == 0 else ":")
+    print(ar)
+    for i in range(1, arg_len + 1):
         print("{}: {}".format(i, sys.argv[i]))
